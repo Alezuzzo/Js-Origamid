f84ff123cb053ca3b64403a94526000284e6805a
Alezuzzo <Thallesander@hotmail.com>
2021-07-22 20:38:49 -0300

    teste de events

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const linkExterno = document.querySelector('a[href^="http"]');
-function handleLinkExterno(event) {
-    event.preventDefault();
+const h1 = document.querySelector('h1');
+
+function handleEvent(event) {
+    console.log(event.type, event);
 }
 
-linkExterno.addEventListener('click', handleLinkExterno);
+h1.addEventListener('click', handleEvent);
+h1.addEventListener('mouseenter', handleEvent);
+h1.addEventListener('mousemove', handleEvent);
+
+window.addEventListener('scroll', handleEvent);
+window.addEventListener('resize', handleEvent);
+window.addEventListener('keydown', handleEvent);
